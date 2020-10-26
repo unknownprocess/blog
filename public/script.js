07d6a3c916f4ceca3f3e29bb42162a781f0cfa4b
unknownprocess <dimuzaffar@gmail.com>
2020-10-26 16:49:04 +0530

    final update for now till new blog ready

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['hell0 fri3nd.', 'follow me on twitter @unknownprocess', 'New Blog Coming Soon...'], 'text',['tomato','rebeccapurple','lightblue']);
+ consoleText(['hell0 fri3nd.', 'follow me @unknownprocess', 'New Blog Coming Soon...'], 'text',['tomato','rebeccapurple','lightblue']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
